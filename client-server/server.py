31e1097c526e7102304be515362559730c10dfc0
Merada Richter <merada.richter@gmail.com>
2014-03-12 21:55:55 +0200

    Bootstrap set with Slate theme
    Configuration file (settings.py)
    Display of search results

--- client-server/server.py
+++ client-server/server.py
@@ -1,29 +1,31 @@
+import json
+import settings
+import solr
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask_bootstrap import Bootstrap
 
+# app setup
 app = Flask(__name__)
 app.debug = True
 Bootstrap(app)
 
+# solr setup
+s = solr.SolrConnection(settings.SOLR_URL)
+
+#----- PAGES -------------------------------------
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    results = "RESULT"
     if request.method == 'POST':
-        if request.form['query'] == "dragon":
-            return "Here be dragons"
-        else:
-            return "No dragons be here"
-    return render_template('home.html', result=results)
-
-@app.route("/dragon")
-def merada():
-    return "<button>scales</button>"
+        results = s.query(request.form['query']) # basic query
+    else:
+        results = get_results() # test file
+    return render_template('search.html', results=get_results())
 
 @app.route("/test")
 def test_bootstrap():
@@ -37,5 +39,10 @@ def test_layout():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+#----- HELPERS ------------------------------------
+def get_results():
+    with open("./results.json") as f:
+        return json.load(f)['response']['docs']
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port = settings.SERVER_PORT)
---
+++ client-server/settings.py
@@ -0,0 +1,2 @@
+SOLR_URL="http:somewhere"
+SERVER_PORT=8888
