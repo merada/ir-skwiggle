0b181add7fe7d7a4d09243f3514a84e75d250c01
Merada Richter <merada.richter@gmail.com>
2014-03-13 10:36:42 +0200

    Fixes link between home and search
    Adds query search method

--- client-server/server.py
+++ client-server/server.py
@@ -15,18 +15,27 @@ Bootstrap(app)
 s = solr.SolrConnection(settings.SOLR_URL)
 
 #----- PAGES -------------------------------------
-@app.route("/")
-@app.route("/home")
-def home():
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def home(): 
     return render_template('home.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
-        results = s.query(request.form['query']) # basic query
+        results = s.query(request.form['query'])
     else:
         results = get_results() # test file
     return render_template('search.html', results=results)
+
+@app.route('/search#<query>', methods=['GET', 'POST'])
+def search_query():
+    results = s.query(query)
+    return render_template('search.html', results=results)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
